0e0453c61b90d940050675fdadf5f04fcc7e605d
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-03 10:09:33 +0600

    Add files via upload

---
+++ CSES Problem Sets/Number Spiral.cpp
@@ -0,0 +1,33 @@
+ /*******************বিসমিল্লাহির রাহমানির রাহিম **********************/
+ /*__________________ MD ASADUZZAMAN SHUVO ______________________ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int INF = 1e9+10;
+const int N = 1e5+10;
+
+int main()
+{
+ int t;cin>>t;
+   
+    while(t--){
+        ll row,column;cin>> row>>column;
+
+        if(row>column){
+            if(row%2 == 0){
+                cout<< row*row - (column - 1)<<endl;
+            }
+            else cout<< (row-1)*(row -1 ) +1+ (column - 1)<<endl;
+        }else{
+               if(column%2 != 0){
+                cout<< column*column - (row - 1)<<endl;
+            }
+            else cout<< (column-1)*(column -1 ) +1+ (row - 1)<<endl;
+        }
+
+    }
+
+    return 0;
+}
