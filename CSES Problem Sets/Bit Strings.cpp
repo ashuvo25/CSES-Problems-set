79b8be36f164478392ef754d56cc8c7d71788af3
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-05 22:19:02 +0600

    Add files via upload

---
+++ CSES Problem Sets/Bit Strings.cpp
@@ -0,0 +1,20 @@
+ /*******************বিসমিল্লাহির রাহমানির রাহিম **********************/
+ /*__________________ MD ASADUZZAMAN SHUVO ______________________ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int INF = 1e9+7;
+const int N = 1e5+10;
+
+int main()
+{
+ int t;cin>>t;
+  ll n = 1;
+  for(int i = 0 ;i<t;i++){
+    n = (n*2)%INF;
+  }
+ cout << n <<endl;
+    return 0;
+}
